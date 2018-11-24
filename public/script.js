45268e12df93b5c0646a52c57319d4e5ff963ba7
Mauri <hello@mauriciabad.com>
2018-11-24 20:41:34 +0100

    efd

--- public/script.js
+++ public/script.js
@@ -5,7 +5,13 @@
 
 
 
+var paper = new Raster('paper');
+paper.position = view.center;
 
+function onResize(event) {
+	// Whenever the window is resized, recenter the path:
+	paper.position = view.center;
+}
 
 
 
