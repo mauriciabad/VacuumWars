bca1e9ef74f621a7804ecc70f43d3c19fdfa35fc
Mauri <hello@mauriciabad.com>
2018-11-25 10:56:19 +0100

    viewerPlays

--- public/script.js
+++ public/script.js
@@ -4,6 +4,7 @@ window.onload = function() {
 
 
 
+  var viewerPlays = false;
   var socket   = io();
   var trashes  = {}; 
   var powerUps = {};
@@ -129,49 +130,50 @@ window.onload = function() {
   });
   
 
-  // this player movement
-  // var tool = new paper.Tool();
-  // tool.onKeyDown = function(event) {
-  //   switch (event.key) {
-  //     case 'space':
-  //     move(true, event);
-  //       break;
-  //     case 'enter':
-  //     action(true, event);
-  //       break;
-  //   }
-  //   return false;
-  // }
-  // tool.onKeyUp = function(event) {
-  //   switch (event.key) {
-  //     case 'space':
-  //     move(false, event);
-  //       break;
-  //     case 'enter':
-  //     action(false, event);
-  //       break;
-  //   }
-  //   return false;
-  // }
+  if(viewerPlays){
+    var tool = new paper.Tool();
+    tool.onKeyDown = function(event) {
+      switch (event.key) {
+        case 'space':
+        move(true, event);
+          break;
+        case 'enter':
+        action(true, event);
+          break;
+      }
+      return false;
+    }
+    tool.onKeyUp = function(event) {
+      switch (event.key) {
+        case 'space':
+        move(false, event);
+          break;
+        case 'enter':
+        action(false, event);
+          break;
+      }
+      return false;
+    }
 
-  // function move(isMoving, e) {
-  //   console.log('isMoving', isMoving);
-  //   e.preventDefault();
-  //   socket.emit('move', isMoving);
-  // }
-  // function action(isActing, e) {
-  //   console.log('isActing', isActing);
-  //   e.preventDefault();
-  //   socket.emit('active', isActing);
-  // }
-  // function rename(name) {
-  //   socket.emit('rename', name);      
-  // }
-  // function changeVacuum(vacuum) {
-  //   socket.emit('changeVacuum', vacuum);      
-  // }
-  socket.emit('ignoreMe', true);      
-
+    function move(isMoving, e) {
+      console.log('isMoving', isMoving);
+      e.preventDefault();
+      socket.emit('move', isMoving);
+    }
+    function action(isActing, e) {
+      console.log('isActing', isActing);
+      e.preventDefault();
+      socket.emit('active', isActing);
+    }
+    function rename(name) {
+      socket.emit('rename', name);      
+    }
+    function changeVacuum(vacuum) {
+      socket.emit('changeVacuum', vacuum);      
+    }
+  } else {
+    socket.emit('ignoreMe', true);      
+  }
 
 
 
